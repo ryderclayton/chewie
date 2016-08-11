2536e3cfa252aa3ceb3544cae5125614c4d4a6eb
Ryder Clayton <ryder_clayton@yahoo.com>
2016-08-11 13:02:04 +0000

    Edited user permissions to allow only the creator access to edit and destroy a place.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.order("created_at ASC").all.paginate(:page => params[:page], :per_page => 5)
@@ -20,16 +20,29 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :unauthorized
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :unauthorized
+    end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
 
   def destroy
-    @place = Place.find(params[:id]).destroy
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
+    @place.destroy
     redirect_to root_path
   end
 
