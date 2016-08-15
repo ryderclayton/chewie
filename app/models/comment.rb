83cea68e40665c554bb7144883237d251681fa2c
Ryder Clayton <ryder_clayton@yahoo.com>
2016-08-15 12:15:24 +0000

    Converted the rating textbox into a drop down.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    '1 star' => '1_star',
+    '2 stars' => '2_stars',
+    '3 stars' => '3_stars',
+    '4 stars' => '4_stars',
+    '5 stars' => '5_stars',
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
